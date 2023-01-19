af66c7b8b81c55b52cdd00d15a5e659e8d2fd5d7
jesselizcb <choque101108@gmail.com>
2023-01-18 20:06:00 -0500

    first commit

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-card class="mx-auto" max-width="344">
+      <v-img
+        :src="datos.sprites.other.home.front_default"
+      ></v-img>
+      <v-card-title>{{ datos.name }}</v-card-title>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: "pokemon",
+  props: {
+    pokemonItem: Object
+  },
+  data: () => ({
+    datos: {}
+  }),
+  mounted() {
+    axios.get(this.pokemonItem.url).then((res) => {
+      this.datos = res.data
+      console.log(this.datos)
+    })
+  }
+};
+</script>
